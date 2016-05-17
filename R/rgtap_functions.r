264ead3f64a9fb10838f0838ddd38f6e2babbdd1
nvilloria <nvilloria@gmail.com>
2016-05-16 21:23:30 -0500

    various changes

--- R/rgtap_functions.r
+++ R/rgtap_functions.r
@@ -1,12 +1,26 @@
 #' Extract variables for GEMPACK solution file(s).
 #'
-#' @param solution.dir Directory where the solution file is stored.
-#' @param solution.name Name of the solution, including suffix
-#' @param var.map A GEMPACK variable map to be used with "sltoht". Need to add a reference to the GTAP manual.
-#' @param solution.out It's the name (or directory?) where you put the solution
+#' \code{extractvar} uses GEMPACK's \code{sltoht} to extract the
+#' variables defined in var.map from a GEMPACK solution file (SL4:
+#' suffixed .sl4) and writes them to a HAR file (generally suffixed
+#' .sol).
+#'
+#' @param solution.dir Directory where the solution file SL4 is stored.
+#' @param solution.name Name of the solution file SL4, without a suffix.
+#' @param var.map A GEMPACK variable map to be used with
+#' \code{sltoht}. See
+#' \url{http://www.copsmodels.com/gpmanual.htm#gpd4.8} for details
+#' about \code{sltoht}.
+#' @param solution.out Path and name of the HAR with the variables
+#' from var.map. Must include a suffix, preferable .sol.
+#' @return A HAR file with variables in var.map.
 #' @export
-#' @examples Examples
-#' extractvar()
+#' @examples Examples:
+#' \dontrun{ extractvar(solution.dir = "d:/nvilloria/Dropbox/papers/lusupply/SC2010Biofuels/",
+#' solution.name = "Long",
+#' var.map = "SC2010Biofuels.map",
+#' solution.out = "../Results/Long.sol")
+#' }
 extractvar <- function (solution.dir, solution.name, var.map, solution.out){
     system(
            paste("sltoht",
